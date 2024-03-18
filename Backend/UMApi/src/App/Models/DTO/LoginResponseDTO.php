1246878aee5f84c71085a883b71988e4e8ef12a3
Aleksandar_Tulic <39736298+AleksandarTulic@users.noreply.github.com>
2024-03-18 15:25:44 +0100

    Create LoginResponseDTO.php

---
+++ Backend/UMApi/src/App/Models/DTO/LoginResponseDTO.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\DTO;
+
+final class LoginResponseDTO{
+    public string $accessToken;
+    public string $refreshToken;
+
+    public function __construct(string $accessToken, string $refreshToken){
+        $this->accessToken = $accessToken;
+        $this->refreshToken = $refreshToken;
+    }
+
+}
+
+?>
